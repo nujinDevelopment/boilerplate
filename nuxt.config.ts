c4ab9a59d4712bb2a1f1566c16e48947f57efbfe
christonomous <chris@brane.media>
2024-09-06 12:24:43 +0200

    extend base

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,40 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: 'nujin Boilerplate',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+
+  supabase: {
+    redirect: false,
+    redirectOptions: {
+      // login: '/login',
+      // callback: '/callback'
+    }
+  },
+
+  runtimeConfig: {
+    public: {
+      // supabaseUrl: process.env.SUPABASE_URL,
+      // supabaseKey: process.env.SUPABASE_KEY
+    }
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/supabase',
