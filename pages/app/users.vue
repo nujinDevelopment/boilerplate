c9f0b46a8a587af469920179f4a1823e8242b939
christonomous <chris@brane.media>
2024-09-12 11:24:26 +0200

    facelift

--- pages/app/users.vue
+++ pages/app/users.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-4">
-    <h1 class="text-2xl font-bold mb-6">User Management</h1>
     <div v-if="error" class="alert alert-error mb-6">{{ error }}</div>
     <UserManagementUserStats v-if="showStats && canViewStats" class="mb-6" />
     <div class="grid grid-cols-1 xl:grid-cols-3 gap-6">
